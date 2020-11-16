eba1d3a49c1e5f3b6fbc99748325336ceb07c650
benoit17630 <benoit.menier@orange.fr>
2020-11-16 13:08:13 +0100

    ajout de la page poker

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -31,8 +31,21 @@ class PageController
 
        $age = $request->query->get('age');
        $nom = $request->query->get('name');// je creer la variable $nom et je recupere le name dans l url
-        var_dump($nom);
-        var_dump($age);
+       echo "bonjour : ".$nom ." et tu a ".$age."ans";
+
         die;
+    }
+    /**
+     * @route("/poker", name="poker_page")
+     * @param Request $request
+     */
+    public function poker(Request $request){
+        //je cree une metode poker avec l url /poker
+        $age = $request->get('age');
+        if($age >=18){
+            die( "tu peut aceder au jeu de poker");//si plus de 18 ans j afiche se message
+        }else{
+            die("tu na pas l age pour jouer");//si moin de 18ans se message
+        }
     }
 }
